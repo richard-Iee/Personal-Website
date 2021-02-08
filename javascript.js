c2e353273417290a854bb0a5e3fdf6e8e21d9a02
richard875 <richard_875@me.com>
2021-02-08 18:23:21 +1300

    added darkmode

--- javascript.js
+++ javascript.js
@@ -10,40 +10,35 @@ var myViewModel = {
 
 ko.applyBindings(myViewModel);
 
-if ($(this).scrollTop() == 0) {
-  $(".fa-user").fadeIn(200);
-} else {
-  $(".fa-user").css("display", "none");
-}
+$(this).scrollTop() == 0
+  ? $(".fa-user").fadeIn(200)
+  : $(".fa-user").css("display", "none");
 
-var toHome = () => {
-  $("html, body").animate({ scrollTop: 0 }, 100);
-};
+var toHome = () => $("html, body").animate({ scrollTop: 0 }, 100);
 
-var internExperienceAndProjects = () => {
+var introduction = () =>
+  $("html, body").animate({ scrollTop: $("center").offset().top }, 100);
+
+var internExperienceAndProjects = () =>
   $("html, body").animate(
     { scrollTop: $("#internExperienceAndProjects").offset().top - 100 },
     100
   );
-};
 
-var volunteeringAndOthers = () => {
+var volunteeringAndOthers = () =>
   $("html, body").animate(
     { scrollTop: $("#volunteeringAndOthers").offset().top - 100 },
     100
   );
-};
 
-var personalStatements = () => {
+var personalStatements = () =>
   $("html, body").animate(
     { scrollTop: $("#personalStatements").offset().top - 100 },
     100
   );
-};
 
-var toBottom = () => {
+var toBottom = () =>
   $("html, body").animate({ scrollTop: $(document).height() }, 100);
-};
 
 // When the user scrolls the page, execute function
 window.onscroll = () => {
@@ -65,75 +60,54 @@ window.onscroll = () => {
   var cars = $("#cars").offset().top;
   var contact = $("#contact").offset().top;
 
-  if (scrollPosition < internExperienceAndProjects) {
-    $(".fa-user").fadeIn();
-  } else {
-    $(".fa-user").css("display", "none");
-  }
-  if (
-    scrollPosition >= internExperienceAndProjects &&
-    scrollPosition < skills
-  ) {
-    $(".fa-lightbulb").fadeIn();
-  } else {
-    $(".fa-lightbulb").css("display", "none");
-  }
-  if (scrollPosition >= skills && scrollPosition < volunteeringAndOthers) {
-    $(".fa-star").fadeIn();
-  } else {
-    $(".fa-star").css("display", "none");
-  }
-  if (scrollPosition >= volunteeringAndOthers && scrollPosition < employments) {
-    $(".fa-hands-helping").fadeIn();
-  } else {
-    $(".fa-hands-helping").css("display", "none");
-  }
-  if (scrollPosition >= employments && scrollPosition < personalStatements) {
-    $(".fa-building").fadeIn();
-  } else {
-    $(".fa-building").css("display", "none");
-  }
-  if (scrollPosition >= personalStatements && scrollPosition < education) {
-    $(".fa-info").fadeIn();
-  } else {
-    $(".fa-info").css("display", "none");
-  }
-  if (scrollPosition >= education && scrollPosition < cars) {
-    $(".fa-university").fadeIn();
-  } else {
-    $(".fa-university").css("display", "none");
-  }
-  if (scrollPosition >= cars && scrollPosition < contact - 800) {
-    $(".fa-car").fadeIn();
-  } else {
-    $(".fa-car").css("display", "none");
-  }
-  if (scrollPosition >= contact - 799) {
-    $(".special-envelope").fadeIn();
-  } else {
-    $(".special-envelope").css("display", "none");
-  }
+  scrollPosition < internExperienceAndProjects
+    ? $(".fa-user").fadeIn()
+    : $(".fa-user").css("display", "none");
 
-  if ($(this).scrollTop() > $(document).height() * 0.04) {
-    $(".backToTop").fadeIn();
-  } else {
-    $(".backToTop").fadeOut();
-  }
+  scrollPosition >= internExperienceAndProjects && scrollPosition < skills
+    ? $(".fa-lightbulb").fadeIn()
+    : $(".fa-lightbulb").css("display", "none");
 
-  if (
-    $(this).scrollTop() > $(document).height() * 0.02 &&
-    $(this).scrollTop() < $(document).height() * 0.9
-  ) {
-    $(".toBottom").fadeIn();
-  } else {
-    $(".toBottom").fadeOut();
-  }
+  scrollPosition >= skills && scrollPosition < volunteeringAndOthers
+    ? $(".fa-star").fadeIn()
+    : $(".fa-star").css("display", "none");
 
-  if ($(this).scrollTop() > $(document).height() * 0.04) {
-    $(".mobile-util-menu").css("right", "0vw");
-  } else {
-    $(".mobile-util-menu").css("right", "-15vw");
-  }
+  scrollPosition >= volunteeringAndOthers && scrollPosition < employments
+    ? $(".fa-hands-helping").fadeIn()
+    : $(".fa-hands-helping").css("display", "none");
+
+  scrollPosition >= employments && scrollPosition < personalStatements
+    ? $(".fa-building").fadeIn()
+    : $(".fa-building").css("display", "none");
+
+  scrollPosition >= personalStatements && scrollPosition < education
+    ? $(".fa-info").fadeIn()
+    : $(".fa-info").css("display", "none");
+
+  scrollPosition >= education && scrollPosition < cars
+    ? $(".fa-university").fadeIn()
+    : $(".fa-university").css("display", "none");
+
+  scrollPosition >= cars && scrollPosition < contact - 800
+    ? $(".fa-car").fadeIn()
+    : $(".fa-car").css("display", "none");
+
+  scrollPosition >= contact - 799
+    ? $(".special-envelope").fadeIn()
+    : $(".special-envelope").css("display", "none");
+
+  $(this).scrollTop() > $(document).height() * 0.04
+    ? $(".backToTop").fadeIn()
+    : $(".backToTop").fadeOut();
+
+  $(this).scrollTop() > $(document).height() * 0.02 &&
+  $(this).scrollTop() < $(document).height() * 0.9
+    ? $(".toBottom").fadeIn()
+    : $(".toBottom").fadeOut();
+
+  $(this).scrollTop() > $(document).height() * 0.04
+    ? $(".mobile-util-menu").css("right", "0vw")
+    : $(".mobile-util-menu").css("right", "-15vw");
 
   var top = $(window).scrollTop() + $(window).height(),
     isVisible = top > $(".skill-set").offset().top;
@@ -141,9 +115,7 @@ window.onscroll = () => {
   $(".skill-set").toggleClass("animate", isVisible);
 };
 
-var starNumber = () => {
-  return 10 + Math.floor(Math.random() * Math.floor(30));
-};
+var starNumber = () => 10 + Math.floor(Math.random() * Math.floor(30));
 
 var starSpecs = (min, max) => {
   min = Math.ceil(min);
@@ -151,60 +123,57 @@ var starSpecs = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-var starPosition = (max) => {
-  return Math.floor(Math.random() * Math.floor(max));
-};
+var starPosition = (max) => Math.floor(Math.random() * Math.floor(max));
 
 for (var i = 0; i <= starNumber(); i++) {
   starDimensions = starSpecs(100, 500) / 1000;
-  starXPos = starPosition(500) / 10;
+  starXPos = starPosition(1000);
   starYPos = starPosition(1000) / 10;
   $(".frontLeftPic").prepend(
-    `<div class="stars blink d-none d-xl-flex" style="top: ${starYPos}%; left: ${starXPos}%; width: ${starDimensions}vw; height: ${starDimensions}vw"></div>`
+    `<div class="stars blink d-none d-xl-flex" style="top: ${starYPos}vh; left: ${starXPos}px; width: ${starDimensions}vw; height: ${starDimensions}vw"></div>`
   );
 }
 
-$(".scrollDown").click(function () {
-  $("html, body").animate(
-    { scrollTop: $("#internExperienceAndProjects").offset().top },
-    100
-  );
-});
+$(".scrollDown").click(() => introduction());
 
-$(".catchUp").click(function () {
-  $("html, body").animate({ scrollTop: $(document).height() }, 100);
-});
+$(".catchUp").click(() =>
+  $("html, body").animate({ scrollTop: $(document).height() }, 100)
+);
 
 var mobileDevice = {
-  Android: function () {
-    return navigator.userAgent.match(/Android/i);
-  },
-  BlackBerry: function () {
-    return navigator.userAgent.match(/BlackBerry/i);
-  },
-  iOS: function () {
-    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-  },
-  Opera: function () {
-    return navigator.userAgent.match(/Opera Mini/i);
-  },
-  Windows: function () {
-    return navigator.userAgent.match(/IEMobile/i);
-  },
-  any: function () {
-    return (
-      mobileDevice.Android() ||
-      mobileDevice.BlackBerry() ||
-      mobileDevice.iOS() ||
-      mobileDevice.Opera() ||
-      mobileDevice.Windows()
-    );
-  },
+  Android: () => navigator.userAgent.match(/Android/i),
+  BlackBerry: () => navigator.userAgent.match(/BlackBerry/i),
+  iOS: () => navigator.userAgent.match(/iPhone|iPad|iPod/i),
+  Opera: () => navigator.userAgent.match(/Opera Mini/i),
+  Windows: () => navigator.userAgent.match(/IEMobile/i),
+  any: () =>
+    mobileDevice.Android() ||
+    mobileDevice.BlackBerry() ||
+    mobileDevice.iOS() ||
+    mobileDevice.Opera() ||
+    mobileDevice.Windows(),
 };
 
 /* if the device is not ios hide the download button */
-if (mobileDevice.iOS()) {
-  $(".mobile-back-to-top").css("visibility", "hidden");
+if (mobileDevice.iOS()) $(".mobile-back-to-top").css("visibility", "hidden");
+
+const toggle = document.querySelector(".dark-mode-button");
+toggle.checked = false;
+
+if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  toggle.checked = true;
+
+  document
+    .querySelector("meta[name='twitter:widgets:theme']")
+    .setAttribute("content", "dark");
+
+  $("#dark-mode").addClass("dark-mode-theme");
 }
+
+$(".dark-mode-button").click(() => {
+  $("#dark-mode").hasClass("dark-mode-theme")
+    ? $("#dark-mode").removeClass()
+    : $("#dark-mode").addClass("dark-mode-theme");
+});
 
 AOS.init();
